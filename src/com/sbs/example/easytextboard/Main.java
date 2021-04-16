eba8cd322c21996b6b1218021fafdb5063dc6054
ysh9698 <ysh9698@naver.com>
2021-04-16 18:18:24 +0900

    고객이 종료를 원할 때 까지 실행되도록

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -6,20 +6,22 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.printf("명렁어)");
+	    while ( true ) {
+	    	
+	    System.out.printf("명령어) ");
 		String command = scanner.nextLine();
 		
-		if ( command.equals("article add")) {
+		if (command.equals("article add")) {
 			System.out.println("== 게시물 등록 ==");
+		} else if (command.equals("article list")) {
+		    System.out.println("== 게시물 리스트 ==");
+		} else if (command.equals("system exit")) {
+			System.out.println("== 프로그램 종료 ==");
+			break;
+		} else {
+		    System.out.println("== 존재하지 않는 명렁어 ==");
 		}
-		else
-		if ( command.equals("article list")) {
-			System.out.println("== 게시물 리스트 ==");
-		}
-		else {
-			System.out.println("== 존재하지 않는 명령어 ==");
-		}
-		
-		scanner.close();
+	  }
+	    scanner.close();
 	}
 }
