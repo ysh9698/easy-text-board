e4bfb2476c78f47219fecd298c54e9a5dcf99b1d
ysh9698 <ysh9698@naver.com>
2021-04-16 17:33:49 +0900

    스캐너로 고객에게 입력받기

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -1,7 +1,25 @@
 package com.sbs.example.easytextboard;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("안녕");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.printf("명렁어)");
+		String command = scanner.nextLine();
+		
+		if ( command.equals("article add")) {
+			System.out.println("== 게시물 등록 ==");
+		}
+		else
+		if ( command.equals("article list")) {
+			System.out.println("== 게시물 리스트 ==");
+		}
+		else {
+			System.out.println("== 존재하지 않는 명령어 ==");
+		}
+		
+		scanner.close();
 	}
 }
