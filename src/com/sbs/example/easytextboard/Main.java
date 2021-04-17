f892056ddd4d6806de5a563ff12ee3aaf4912fa9
ysh9698 <ysh9698@naver.com>
2021-04-17 13:18:18 +0900

    게시물 등록 시 정보입력받는 기능 구현

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -4,24 +4,42 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	    Scanner scanner = new Scanner(System.in);
 		
-	    while ( true ) {
+	    while (true) {
 	    	
-	    System.out.printf("명령어) ");
+		System.out.printf("명령어) ");
 		String command = scanner.nextLine();
 		
 		if (command.equals("article add")) {
-			System.out.println("== 게시물 등록 ==");
-		} else if (command.equals("article list")) {
-		    System.out.println("== 게시물 리스트 ==");
-		} else if (command.equals("system exit")) {
+			System.out.println("== 게시물 작성 ==");
+			
+			int id = 1;
+			String title;
+			String body;
+			
+			System.out.printf("제목 : ");
+			title = scanner.nextLine();
+			System.out.printf("내용 : ");
+			body = scanner.nextLine();
+			
+			System.out.println("== 생성된 게시물 정보 ==");
+			System.out.println("번호 : " + id);
+			System.out.println("제목 : " + title);
+			System.out.println("내용 : " + body);
+		}
+		else if (command.equals("article list")) {
+			System.out.println("== 게시물 리스트 ==");
+		}
+		else if (command.equals("system exit")) {
 			System.out.println("== 프로그램 종료 ==");
 			break;
-		} else {
-		    System.out.println("== 존재하지 않는 명렁어 ==");
 		}
-	  }
+		else {
+			System.out.println("== 존재하지 않는 게시물 ==");
+		}
+	    }
+	    
 	    scanner.close();
 	}
 }
