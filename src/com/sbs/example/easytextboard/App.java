a3a3d7e1b7352b6a3b0e6be2ff6f0e728ecd1c93
ysh9698 <ysh9698@naver.com>
2021-04-28 17:48:53 +0900

    생성자 메서드 문법으로 init 함수 제거

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -7,7 +7,7 @@ public class App {
 	private int lastArticleId;
 	private int articlesSize;
 	
-	public void init() {
+	public App() {
 		articles = new Article[32];
 		lastArticleId = 0;
 		articlesSize = 0;
--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -3,7 +3,6 @@ package com.sbs.example.easytextboard;
 public class Main {
 	public static void main(String[] args) {
 		App app = new App();
-		app.init();
 		app.run();
 	}
 }
